266998b460da70f9b0ca308daf6c0c1a1abf8a9e
AlvesM97 <gutomorais2002@gmail.com>
2023-10-03 09:59:17 -0300

    Update sunshine.service.js

--- assets/services/sunshine.service.js
+++ assets/services/sunshine.service.js
@@ -7,10 +7,19 @@
       var client = ZAFClient.init();
 
       return {
-        consultarObjetoDeRegistroPorTipo: function (baseUrl) {
+        consultarObjetoDeRegistroPorRelacionamento: function (
+          baseUrl,
+          idUsuario,
+          nomeRelacionamento
+        ) {
           var deferred = $q.defer();
           let options = {
-            url: baseUrl + "/api/sunshine/objects/records?type=blocodenotas",
+            url:
+              baseUrl +
+              "/api/sunshine/objects/records/zen:user:" +
+              idUsuario +
+              "/related/" +
+              nomeRelacionamento,
             type: "GET",
             contentType: "application/json",
           };
@@ -25,11 +34,75 @@
           return deferred.promise;
         },
 
-        criarObjetoDeRegistroPorTipo: function (baseUrl) {
+        criarObjetoDeTipo: function (baseUrl, nomeTipoObjeto) {
+          var deferred = $q.defer();
+          var dataObjetoDeTipo = {
+            data: {
+              key: nomeTipoObjeto,
+              schema: {
+                properties: {
+                  dados: {
+                    type: "string",
+                    description: "dados anotados",
+                  },
+                },
+              },
+            },
+          };
+
+          let options = {
+            url: baseUrl + "/api/sunshine/objects/types",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(dataObjetoDeTipo),
+          };
+          client
+            .request(options)
+            .then(function (data) {
+              deferred.resolve(data);
+            })
+            .catch(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        },
+
+        criarTipoDeRelacionamento: function (
+          baseUrl,
+          nomeRelacionamento,
+          nomeTipoObjeto
+        ) {
+          var deferred = $q.defer();
+          var dataTipoDeRelacionamento = {
+            data: {
+              key: nomeRelacionamento,
+              source: "zen:user",
+              target: nomeTipoObjeto,
+            },
+          };
+
+          let options = {
+            url: baseUrl + "/api/sunshine/relationships/types",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(dataTipoDeRelacionamento),
+          };
+          client
+            .request(options)
+            .then(function (data) {
+              deferred.resolve(data);
+            })
+            .catch(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        },
+
+        criarObjetoDeRegistroPorTipo: function (baseUrl, nomeTipoObjeto) {
           var deferred = $q.defer();
           var dataObjetoDeRegistroPorTipo = {
             data: {
-              type: "blocodenotas",
+              type: nomeTipoObjeto,
               attributes: {
                 dados: "",
               },
@@ -53,47 +126,26 @@
           return deferred.promise;
         },
 
-        consultarObjetoDeRegistroPorRelacionamento: function (
+        criarObjetoDeRegistro: function (
           baseUrl,
-          idUsuario
+          idObjetoDeRegistro,
+          idUsuario,
+          nomeRelacionamento
         ) {
+          var dataObjetoDeRegistro = {
+            data: {
+              relationship_type: nomeRelacionamento,
+              source: "zen:user:" + idUsuario,
+              target: idObjetoDeRegistro,
+            },
+          };
+
           var deferred = $q.defer();
           let options = {
             url:
               baseUrl +
-              "/api/sunshine/objects/records/zen:user:" +
-              idUsuario +
-              "/related/user_nota",
-            type: "GET",
-            contentType: "application/json",
-          };
-          client
-            .request(options)
-            .then(function (data) {
-              deferred.resolve(data);
-            })
-            .catch(function (error) {
-              deferred.reject(error);
-            });
-          return deferred.promise;
-        },
-
-        criarObjetoDeRegistro: function (
-          baseUrl,
-          idObjetoDeRegistro,
-          idUsuario
-        ) {
-          var dataObjetoDeRegistro = {
-            data: {
-              relationship_type: "user_nota",
-              source: "zen:user:" + idUsuario,
-              target: idObjetoDeRegistro
-            }
-          };
-
-          var deferred = $q.defer();
-          let options = {
-            url: baseUrl + "/api/sunshine/relationships/records?type=user_nota",
+              "/api/sunshine/relationships/records?type=" +
+              nomeRelacionamento,
             type: "POST",
             contentType: "application/json",
             data: JSON.stringify(dataObjetoDeRegistro),
@@ -129,102 +181,6 @@
             type: "PATCH",
             contentType: "application/merge-patch+json",
             data: JSON.stringify(dataObjetoDeRegistro),
-          };
-          client
-            .request(options)
-            .then(function (data) {
-              deferred.resolve(JSON.parse(data));
-            })
-            .catch(function (error) {
-              deferred.reject(error);
-            });
-          return deferred.promise;
-        },
-
-        consultarObjetoDeTipo: function (baseUrl) {
-          var deferred = $q.defer();
-          let options = {
-            url: baseUrl + "/api/sunshine/objects/types/blocodenotas",
-            type: "GET",
-            contentType: "application/json",
-          };
-          client
-            .request(options)
-            .then(function (data) {
-              deferred.resolve(data);
-            })
-            .catch(function (error) {
-              deferred.reject(error);
-            });
-          return deferred.promise;
-        },
-
-        criarObjetoDeTipo: function (baseUrl) {
-          var deferred = $q.defer();
-          var dataObjetoDeTipo = {
-            data: {
-              key: "blocodenotas",
-              schema: {
-                properties: {
-                  dados: {
-                    type: "string",
-                    description: "dados anotados",
-                  },
-                },
-              },
-            },
-          };
-
-          let options = {
-            url: baseUrl + "/api/sunshine/objects/types",
-            type: "POST",
-            contentType: "application/json",
-            data: JSON.stringify(dataObjetoDeTipo),
-          };
-          client
-            .request(options)
-            .then(function (data) {
-              deferred.resolve(data);
-            })
-            .catch(function (error) {
-              deferred.reject(error);
-            });
-          return deferred.promise;
-        },
-
-        consultarTipoDeRelacionamento: function (baseUrl) {
-          var deferred = $q.defer();
-          let options = {
-            url: baseUrl + "/api/sunshine/relationships/types/user_nota",
-            type: "GET",
-            contentType: "application/json",
-          };
-          client
-            .request(options)
-            .then(function (data) {
-              deferred.resolve(data);
-            })
-            .catch(function (error) {
-              deferred.reject(error);
-            });
-          return deferred.promise;
-        },
-
-        criarTipoDeRelacionamento: function (baseUrl) {
-          var deferred = $q.defer();
-          var dataTipoDeRelacionamento = {
-            data: {
-              key: "user_nota",
-              source: "zen:user",
-              target: "blocodenotas",
-            },
-          };
-
-          let options = {
-            url: baseUrl + "/api/sunshine/relationships/types",
-            type: "POST",
-            contentType: "application/json",
-            data: JSON.stringify(dataTipoDeRelacionamento),
           };
           client
             .request(options)
